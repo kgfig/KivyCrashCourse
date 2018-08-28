369e28ad553d82b1edb87af0fe406154af43d4e1
unknown <t4@gmail.com>
2018-08-28 20:39:33 +0800

    Use Kivy language to define widget tree

--- main.py
+++ main.py
@@ -6,29 +6,16 @@ from kivy.uix.label import Label
 from kivy.uix.scatter import Scatter
 from kivy.uix.textinput import TextInput
 
+class ScatterTextWidget(BoxLayout):
+    pass
+
 class TutorialApp(App):
 
     # This method is automatically called when the App is run.
     # Whatever this returns is used as the root widget which is
     # automatically sized and positioned to fill the window.
     def build(self):
-        box = BoxLayout(orientation='vertical')
-        # Always size_hint_y to None so BoxLayout doesn't split the space proportionally and we can set the height manually
-        textInput = TextInput(font_size=150, size_hint_y=None, height=200, text='Default')
-        floatLayout = FloatLayout()
-        scatter = Scatter()
-        scatterText = Label(text="Default", font_size=150)
-
-        # setter returns a function that sets the property with the given key
-        textInput.bind(text=scatterText.setter('text'))
-
-        floatLayout.add_widget(scatter)
-        scatter.add_widget(scatterText)
-
-        box.add_widget(textInput)
-        box.add_widget(floatLayout)
-
-        return box
+        return ScatterTextWidget()
 
 if __name__ == '__main__':
     TutorialApp().run()
