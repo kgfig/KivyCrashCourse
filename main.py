a2223d7c29d916df21ce6c049ad250d8edd16ce8
unknown <t4@gmail.com>
2018-08-28 20:02:55 +0800

    Add a test button in the app

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from kivy.app import App
+from kivy.uix.button import Button
 
 class TutorialApp(App):
-    pass
+
+    # This method is automatically called when the App is run.
+    # Whatever this returns is used as the root widget which is
+    # automatically sized and positioned to fill the window.
+    def build(self):
+        return Button(text='Click me!', background_color=(0, 0, 1, 1), font_size=16)
 
 if __name__ == '__main__':
     TutorialApp().run()
